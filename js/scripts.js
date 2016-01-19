6758fab460e9d0c82decbfc095e4d78de8bdad8d
Afton Downey and Christine Tacha <aaike001@gmail.com>
2016-01-19 14:46:14 -0800

    finish writing specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 var triangle = function(a, b, c) {
-  if (a && b === c) {
-  return true;
-} else if ((a === b && a != c) || (a === c && a != b)) { 
-  return true;
-}
 
+  if ((a + b) <= c || (b + c) <= a || (a + c) <= b) {
+  return false;
+  } else if (a && b === c) {
+   return true;
+  } else if ((a === b && a !== c) || (a === c && a !== b) || (b === c && b !== a)) {
+   return true;
+  } else if ((a !== b) && (b !== c) && (a !== c)) {
+  return true;
+  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,23 @@
 describe ('triangle', function() {
 
+  it("it is false if the sum of 2 sides is <= the third side", function() {
+    expect(triangle(2, 2, 6)).to.equal(false);
+  });
+
   it("it is true if all sides are equal", function() {
-    expect(triangle(3, 3, 3)).to.equal(true);
+   expect(triangle(3, 3, 3)).to.equal(true);
   });
 
   it("it is true if two sides are equal", function() {
-    expect(triangle(3, 3, 2)).to.equal(true);
+   expect(triangle(3, 3, 2)).to.equal(true);
   });
 
   it("it is true if all three sides are different", function() {
-    expect(triangle(3, 2, 1)).to.equal(true);
+    expect(triangle(2, 3, 4)).to.equal(true);
   });
+
+
+  // it("it is a different type of triangle", function() {
+  //   expect()
+  // })
 });
