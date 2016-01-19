f07a32f3a4b62074347ce20a1d90ee64ab829306
Afton Downey and Christine Tacha <aaike001@gmail.com>
2016-01-19 15:20:10 -0800

    add form and declare variables

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,18 @@ var triangle = function(a, b, c) {
   if ((a + b) <= c || (b + c) <= a || (a + c) <= b) {
   return false;
   } else if (a && b === c) {
-   return true;
+   return "equilateral";
   } else if ((a === b && a !== c) || (a === c && a !== b) || (b === c && b !== a)) {
-   return true;
+   return "isosceles";
   } else if ((a !== b) && (b !== c) && (a !== c)) {
-  return true;
+  return "scalene";
   }
 };
+
+$(document).ready(function() {
+  $("form#triangle-tracker").submit(function(event) {
+    var a = parseInt($("input#num1").val());
+    var b = parseInt($("input#num2").val());
+    var c = parseInt($("input#num3").val());
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -5,15 +5,15 @@ describe ('triangle', function() {
   });
 
   it("it is true if all sides are equal", function() {
-   expect(triangle(3, 3, 3)).to.equal(true);
+   expect(triangle(3, 3, 3)).to.equal("equilateral");
   });
 
   it("it is true if two sides are equal", function() {
-   expect(triangle(3, 3, 2)).to.equal(true);
+   expect(triangle(3, 3, 2)).to.equal("isosceles");
   });
 
   it("it is true if all three sides are different", function() {
-    expect(triangle(2, 3, 4)).to.equal(true);
+    expect(triangle(2, 3, 4)).to.equal("scalene");
   });
 
 
