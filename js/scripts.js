79aa2b116bf4b5eee8b2f8949a78f8c6cf6c7de7
Afton Downey and Christine Tacha <tacha.christine@gmail.com>
2016-01-19 15:39:07 -0800

    add final js

--- js/scripts.js
+++ js/scripts.js
@@ -16,5 +16,19 @@ $(document).ready(function() {
     var a = parseInt($("input#num1").val());
     var b = parseInt($("input#num2").val());
     var c = parseInt($("input#num3").val());
+    var result = triangle(a, b, c);
+
+
+    if (!result) {
+      $("#type").hide();
+      $("#not").text("not");
+    } else {
+      $("#not").text("");
+      $("#type").text(result);
+    }
+
+    $("#result").show();
+    event.preventDefault();
+
   });
 });
